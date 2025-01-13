061bde4dfc4258c4b0a19969804b7785bb38ee15
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2025-01-13 17:34:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,21 +18,26 @@ def add_airport():
         st.session_state.airport_names.append(st.session_state.new_airport_name)
         st.session_state.new_airport_name = ""  # Clear input field
 
+# Display current list of airports in a selectbox
+selected_airport = st.selectbox("Select an Airport:", st.session_state.airport_names)
+# Input field and button to add a new airport
+st.text_input("Add a new Airport:", key="new_airport_name")
+st.button("Add Airport", on_click=add_airport)
+
+# Display the selected airport
+st.write(f"You selected: {selected_airport}")
+# Display the updated list of airports
+st.write("Updated list of airports:")
+st.write(st.session_state.airport_names)
+
 # Create a form
 with st.form("input_form"):
     pop_size = st.number_input("Enter your Population Size", min_value=100, max_value=1000)
     num_generations = st.number_input("Enter your Number of Generation", min_value=100, max_value=1000)
     mutation_rate = st.number_input("Enter your Mutation Rate", min_value=0.01, max_value=0.05)
-    # Display current list of airports in a selectbox
-    selected_airport = st.selectbox("Select an Airport:", st.session_state.airport_names)
-    # Input field and button to add a new airport
-    st.text_input("Add a new Airport:", key="new_airport_name")
-    submitted = st.form_submit_button("Add Airport")
-    if submitted:
-        add_airport()
     
     # Submit button inside the form
-    # submitted = st.form_submit_button("Confirm")
+    submitted = st.form_submit_button("Confirm")
 
 # Code after form submission
 if submitted:
@@ -40,7 +45,3 @@ if submitted:
     st.write("Population Size: ", pop_size)
     st.write("Number of Generation: ", num_generations)
     st.write("Mutation Rate: ", mutation_rate) 
-    st.write(f"You selected: {selected_airport}")
-    # Display the updated list of airports
-    st.write("Updated list of airports:")
-    st.write(st.session_state.airport_names)
