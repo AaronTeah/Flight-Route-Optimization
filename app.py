51a1d21689d69cc9cfc9f6f440ec1e2cfb4dc173
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2025-01-13 20:59:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,20 @@ airport_data = pd.read_csv(file_path)
 
 # Create a form widget
 with st.form("airport_form"):
-    st.write("Select airports from the list below:")
-
+    
     # Create checkboxes for each airport
+    st.write("Select airports from the list below:")
     selected_airports = []
     for airport in airport_data['Airport'][1:]:
         if st.checkbox(airport, key=airport):
             selected_airports.append(airport)
 
+    # Set the parameter
+    pop_size = st.number_input("Enter your Population Size", min_value=100, max_value=1000)
+    num_generations = st.number_input("Enter your Number of Generation", min_value=100, max_value=1000)
+    mutation_rate = st.number_input("Enter your Mutation Rate", min_value=0.01, max_value=0.05)
+
+    
     # Submit button
     submitted = st.form_submit_button("Submit")
 
@@ -27,7 +33,6 @@ with st.form("airport_form"):
         # Extract the first airport
         first_airport = airport_data.iloc[0]
         selected_airports_data = airport_data[airport_data['Airport'].isin(selected_airports)]
-
         # Combine the first airport and selected airports
         result_df = pd.concat([first_airport.to_frame().T, selected_airports_data], ignore_index=True)
 
@@ -37,18 +42,7 @@ with st.form("airport_form"):
         st.write("You selected the following airports:")
         st.write(result_df)
         st.success(f"CSV file created successfully: {result_csv_path}")
-
-# Create a form
-with st.form("input_form"):
-    pop_size = st.number_input("Enter your Population Size", min_value=100, max_value=1000)
-    num_generations = st.number_input("Enter your Number of Generation", min_value=100, max_value=1000)
-    mutation_rate = st.number_input("Enter your Mutation Rate", min_value=0.01, max_value=0.05)
-    
-    # Submit button inside the form
-    submitted = st.form_submit_button("Confirm")
-
-    # Code after form submission
-    if submitted:
+        # Print the parameters
         st.write("You have confirmed the parameters!")
         st.write("Population Size: ", pop_size)
         st.write("Number of Generation: ", num_generations)
