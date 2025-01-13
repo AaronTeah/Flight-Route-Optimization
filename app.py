ddeffc0cdf20ea3b58cb0a0e072e5a3153e79917
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2025-01-13 17:11:09 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+#streamlit page title and header setting
+st.set_page_config(
+  page_title="TV Show Scheduling System"
+)
+st.header("TV Show Scheduling System", divider="gray") 
