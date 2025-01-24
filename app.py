41ac7a0ef11b01bae8d429dc593b50a836ec70cb
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2025-01-24 20:44:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 #streamlit page title and header setting
 st.set_page_config( 
-  page_title="Flight Route Optimization", 
-  layout="wide"
+  page_title="Flight Route Optimization"
 )
 
 # Simulate a popup video on app load
